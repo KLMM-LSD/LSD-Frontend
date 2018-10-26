b87f0c2c27da8cdd8aaf79bcbcdc2850a7939560
unknown <lassehansencphbusiness@gmail.com>
2018-10-26 15:13:07 +0200

    Refactor frontpage

---
+++ pages/frontpage.js
@@ -0,0 +1,52 @@
+var threads_id, xhttp, built_string;
+var len, arr_postid, arr_postauthorid, arr_postcontent;
+
+function append_entry(idx)
+{
+	built_string += "<tr><td>"
+	built_string += arr_postid[idx];
+	built_string += "</td><td>"
+	built_string += arr_postcontent[idx];
+	built_string += "</td><td>"
+	built_string += arr_postauthorid[idx];
+	built_string += "</td></tr>";
+}
+
+function fetch_threads()
+{
+	var json;
+
+	if (xhttp.readyState != 4 || xhttp.status != 200)
+		return;
+	
+	json 		= JSON.parse(xhttp.responseText);
+	len		= json["len"];
+	arr_postid	= json["arr_postid"];
+	arr_postauthorid= json["arr_postauthorid"];
+	arr_postcontent	= json["arr_postcontent"];
+
+	built_string = "<table><tr><th>id</th><th>content</th><th>authorid</th></tr>";
+	for (var i = 0; i != len; i++)
+		append_entry(i);
+	built_string += "</table>";
+
+	threads_id.innerHTML = built_string;
+}
+
+function main()
+{
+	threads_id	= document.getElementById("threads");
+	xhttp		= new XMLHttpRequest();
+	built_string	= "Ready<br/>";
+
+	xhttp.onreadystatechange = fetch_threads;
+	xhttp.open("GET", "frontpage.json");
+	xhttp.send();
+}
+
+document.onreadystatechange = function()
+{
+	if (document.readyState !== "interactive")
+		return;
+	main();
+}
